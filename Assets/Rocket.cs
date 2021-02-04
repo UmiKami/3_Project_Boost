0677d507d1895c364f4392c94f4b4373db1f4b0e
UmiKami <ggoku02@gmail.com>
2021-02-04 16:56:02 -0500

    Basic input added, not physicalized yet.

---
+++ Assets/Rocket.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+    void ProcessInput()
+    {
+        if(Input.GetKey(KeyCode.Space)){
+            print("Thorsting");
+        }
+        
+        if(Input.GetKey(KeyCode.A)){
+            print("Rotating left");
+        }else if(Input.GetKey(KeyCode.D)){
+            print("Rotating right");
+        }
+    }
+}
