2a9078a3e8ea06bcc341bd5641e480d10ba33e42
UmiKami <ggoku02@gmail.com>
2021-02-04 21:58:06 -0500

    Rocket sound added, needs improvement

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,22 +6,35 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
+    AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
         ProcessInput();
+        if(Input.GetKeyDown(KeyCode.Space)){
+            audioSource.Play();
+            print("Keydown");
+        }else if(Input.GetKeyUp(KeyCode.Space)){
+            print("Keyup");
+            audioSource.Stop();
+        }
     }
     void ProcessInput()
     {
         if(Input.GetKey(KeyCode.Space)){
             rigidBody.AddRelativeForce(Vector3.up);
+
+            if(!audioSource.isPlaying){
+                audioSource.Play();
+            }
         }
         
         if(Input.GetKey(KeyCode.A)){
